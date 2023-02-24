cc6c84a7f9a9b95c2df3ad70bfeb37d152688484
J4cob-7burg <mr.blox04@gmail.com>
2023-02-24 14:11:53 -0800

    added models

--- videoG/models.py
+++ videoG/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Videog(models.Model):
-    subject = models.CharField(max_length=100)
-    details = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    rating = models.CharField(max_length=2)
+    image = models.CharField(max_length=500)
+    completed = models.CharField(max_length=100)
+    notes = models.CharField(max_length=100)
 
